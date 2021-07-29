4b74e7f167d2f7b18c4f7f144f5cdae2650fa03a
Hamed AMZ <amzhamed@gmail.com>
2021-07-29 12:39:54 +0430

    implement divide multiply and power

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,19 @@
 package calculator;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int power(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result = multiply(result, base);
+        }
+        return result;
     }
 }
